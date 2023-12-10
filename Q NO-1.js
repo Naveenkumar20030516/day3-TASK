976d3cc178d5938642f84189e785845756e73d6f
NAVEEN KUMAR R <142626101+Naveenkumar20030516@users.noreply.github.com>
2023-12-10 19:06:23 +0530

    Add files via upload

---
+++ Q NO-1.js
@@ -0,0 +1,11 @@
+//  To compare two json having the same properties without order in javascript :
+
+//1.How to compare two JSON have the same properties without order?
+//  a. var obj1={name:"Person 1",age:5};
+//  b. var obj2={age:5,name:"Person 1"};
+
+// Solution :
+
+var obj1={name:"Person 1",age:5};
+var obj2={age:5,name:"Person 1"};
+console.log(JSON.stringify(obj1)===JSON.stringify(obj2));
